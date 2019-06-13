5cb2156aa2df0e9520d100144f6cb32e624d39a2
Eddie Teah <eddieteahap@gmail.com>
2019-06-12 22:56:38 -0400

    SetUp Photon 2.0
    With Git HUb Installed In Unity (as an addon)
    In 2019 3.0.a5 Unity (I made a fresh project) And did nothing else Until I
    Download Photon 2.0, Unseleted all Demos
    -I also Delected The Ui and other Utilities Folder

---
+++ NeverRage/Assets/Photon/Script/NetworkController.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Connected" + PhotonNetwork.CloudRegion);
+    }
+}
