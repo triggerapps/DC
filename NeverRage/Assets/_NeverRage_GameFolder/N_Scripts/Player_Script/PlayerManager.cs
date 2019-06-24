e268739a7e8a7b0a574f7c04c75dbc96a87476ea
Eddie Teah <eddieteahap@gmail.com>
2019-06-24 17:03:42 -0400

    I nice Step Forward....
    I can controll my individual camera
    The controller for movement is fine on the main client...but. the other view is effected.

--- NeverRage/Assets/_NeverRage_GameFolder/N_Scripts/Player_Script/PlayerManager.cs
+++ NeverRage/Assets/_NeverRage_GameFolder/N_Scripts/Player_Script/PlayerManager.cs
@@ -1,10 +1,31 @@
-﻿using System.Collections;
+﻿using Photon.Realtime;
+using Photon.Pun;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviourPun,
 {
-    // Start is called before the first frame update
+    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+    public static GameObject LocalPlayerInstance;
+
+    
+
+    void Awake()
+    {
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+        if (photonView.IsMine)
+        {
+            PlayerManager.LocalPlayerInstance = this.gameObject;
+
+        }
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Start()
     {
         
